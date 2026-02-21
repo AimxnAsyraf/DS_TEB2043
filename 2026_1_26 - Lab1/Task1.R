76ce4aad09887199d0a8507848f507846fde8ed9
AimxnAsyraf <89452335+AimxnAsyraf@users.noreply.github.com>
2026-02-21 14:50:19 +0800

    Completed Lab1

---
+++ 2026_1_26 - Lab1/Task1.R
@@ -0,0 +1,7 @@
+num <- 1:20
+
+squares <- num ^ 2
+
+print(squares)
+
+
---
+++ 2026_1_26 - Lab1/Task3.R
@@ -0,0 +1,7 @@
+pie <- 22/7
+
+radius <- as.numeric(readline(prompt = "Enter the radius of the circle: "))
+
+circle_area <- pie * (radius ^ 2)
+
+print(circle_area)
