5f421eff26e5feb5e30429b551d6de78362910a5
AimxnAsyraf <89452335+AimxnAsyraf@users.noreply.github.com>
2026-02-23 10:34:10 +0800

    Completed Lab6 Task2

--- 2026_2_23 - Lab 6/Task2.R
+++ 2026_2_23 - Lab 6/Task2.R
@@ -3,4 +3,7 @@ user.data <- data.frame(
   score = c(12.5, 9.0, 16.5, 12.0, 9.0, 8.0, 19.0),
   attempts = c(1,3,2,3,2,1,2)
 )
-print(user.data)
+
+user.data$qualify <- c("yes", "no", "yes", "no", "no", "no", "yes")
+updated_user.data <- user.data
+print(updated_user.data)
