4b1abe35f435f9c9038f110eea0f08ff4163c51e
AimxnAsyraf <89452335+AimxnAsyraf@users.noreply.github.com>
2026-02-23 10:44:45 +0800

    Completed Lab 6

---
+++ 2026_2_23 - Lab 6/Task4.R
@@ -0,0 +1,22 @@
+user.data <- data.frame(
+  name = c("Anastasia", "Dima", "Michael", "Matthew", "Laura", "Kevin", "Jonas"),
+  score = c(12.5, 9.0, 16.5, 12.0, 9.0, 8.0, 19.0),
+  attempts = c(1,3,2,3,2,1,2)
+)
+
+user.data$qualify <- c("yes", "no", "yes", "no", "no", "no", "yes")
+updated_user.data <- user.data
+
+user.newdata <- data.frame(
+  name = "Emily",
+  score = 14.5,
+  attempts = 1,
+  qualify = "yes"
+)
+
+user.finaldata <- rbind(updated_user.data, user.newdata)
+print(str(user.finaldata))
+print(summary(user.finaldata))
+print(nrow(user.finaldata))
+print(ncol(user.finaldata))
+
