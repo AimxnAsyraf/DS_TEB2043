f0bee42881ad7c02206cbe6187b104584bb86d56
AimxnAsyraf <89452335+AimxnAsyraf@users.noreply.github.com>
2026-02-28 12:46:17 +0800

    Completed Lab4 Task1

--- 2026-02-09_Lab-04/Task1.R
+++ 2026-02-09_Lab-04/Task1.R
@@ -3,7 +3,7 @@ age = c(55, 57, 56, 52, 51, 59, 58, 53, 59, 55, 60, 60, 60, 60, 52, 55, 56, 51,
         51, 50, 57, 58)
 
 age_factor <- factor(age)
-print(levels(age_factor))
+# print(levels(age_factor))
 
 age_table <- table(age_factor)
 # print(age_table)
@@ -18,5 +18,6 @@ age_range <- cut(age,
                  include.lowest = TRUE)
 
 staff_age_range <- table(age_range)
-# print(staff_age_range)
-range_of_staff_age <- data.frame()
+#print(staff_age_range)
+range_of_staff_age <- data.frame(staff_age_range)
+print(range_of_staff_age)
--- 2026-02-16_Assignment-01/Assignment-1.R
+++ 2026-02-16_Assignment-01/Assignment-1.R
@@ -6,21 +6,24 @@ library(readr)
 #Read all lines from uncleaned dataset
 row_data <- read_lines("Unclean Dataset.csv")
 
-#Replace comma, hidden spaces 
+#Replace comma, hidden spaces and remove empty lines
 fixed_lines <- row_data %>%
   str_replace_all("\\|", ",") %>%
   str_remove_all(",+\\s*$")
 
+#Take lines that only has values
 fixed_lines <- fixed_lines[fixed_lines != ""]
 
-
+#Read te corrected data into a dataframe
 dataFrame <- read_csv(paste(fixed_lines, collapse = "\n"), na = c("", "NA", "N/A"))
 # print(dataFrame)
 
+#Standardize headers and trim whitespace
 clean_dataFrame <- dataFrame %>%
   clean_names() %>%
   mutate(across(where(is.character), str_trim))
 
+#Format into correct data type such as double and date
 clean_dataFrame <- clean_dataFrame %>%
   mutate(
     total_payments = parse_number(total_payments),
@@ -28,6 +31,7 @@ clean_dataFrame <- clean_dataFrame %>%
     enrollment_date = as.Date(enrollment_date)  
   )
 
+#Fix inconsistent values or typo errors
 clean_dataFrame <- clean_dataFrame %>%
   mutate(
     course = case_when(
@@ -38,12 +42,15 @@ clean_dataFrame <- clean_dataFrame %>%
     )
   )
 
+#Remove duplicate values
 clean_dataFrame <- clean_dataFrame %>%
   distinct()
 
 clean_dataFrame <- clean_dataFrame %>%
+  #Keep only the rows that has a valid number
   filter(str_detect(student_id, "^[0-9]+$")) %>%
   
+  #Fix the mixed value between student id and total payments
   mutate(
     payment_str = as.character(total_payments),
     id_str = as.character(student_id),
@@ -54,8 +61,10 @@ clean_dataFrame <- clean_dataFrame %>%
       TRUE ~ payment_str
     ),
     
+    #Convert total payment back to numeric
     total_payments = as.numeric(total_payments)
   ) %>%
+  
   
   select(-payment_str, -id_str) %>%
   
@@ -68,5 +77,4 @@ clean_dataFrame <- clean_dataFrame %>%
 cat("Total clean rows remaining:", nrow(clean_dataFrame), "\n")
 print(clean_dataFrame, n = 57)
 
-# Save the absolute final CSV without the row numbers
 write.csv(clean_dataFrame, "Cleaned_Dataset.csv", row.names = FALSE)
